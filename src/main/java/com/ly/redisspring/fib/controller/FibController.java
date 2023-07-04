6d7775adedfbebe6875ba4eec0ae09440f9fc6f3
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-07-04 11:16:58 +0200

    @CacheEvict to clear (remove) members from a hash in redisson

--- src/main/java/com/ly/redisspring/fib/controller/FibController.java
+++ src/main/java/com/ly/redisspring/fib/controller/FibController.java
@@ -14,8 +14,13 @@ public class FibController {
     @Autowired
     private FibService service;
 
-    @GetMapping("/{index}/{name}")
-    public Mono<Integer> getFib(@PathVariable int index, @PathVariable String name){
-        return Mono.fromSupplier(()-> this.service.getFib(index, name));
+    @GetMapping("/{index}")
+    public Mono<Integer> getFib(@PathVariable int index){
+        return Mono.fromSupplier(()-> this.service.getFib(index));
+    }
+
+    @GetMapping("{index}/clear")
+    public Mono<Void> clearCache(@PathVariable int index){
+        return Mono.fromRunnable(()-> this.service.clearCache(index));
     }
 }
--- src/main/java/com/ly/redisspring/fib/service/FibService.java
+++ src/main/java/com/ly/redisspring/fib/service/FibService.java
@@ -1,20 +1,28 @@
 package com.ly.redisspring.fib.service;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FibService {
 
+    //have a stratery for cache evict
     @Cacheable(value = "math:fib", key = "#index")
-    public int getFib(int index, String name){
-        System.out.println("calculating fib for " + index + ", name: " + name); // this will print out every time you call for url. But if we use key = "#index", it will print out every you call url with new index
+    public int getFib(int index){
+        System.out.println("calculating fib for " + index); // this will print out every time you call for url. But if we use key = "#index", it will print out every you call url with new index
         return this.fib(index);
     }
 
+    @CacheEvict(value = "math:fib", key = "#index") //it means whenever this math:fib is invoked, go to this hash"math:fib", with the key = "#index" to clear this
+    public void clearCache(int index){
+        System.out.println("Clearing hash key");
+    }
+
+    //intentional 2^N
     private int fib(int index){
         if(index < 2)
         return index;
-        return  fib(index-1);
+        return  fib(index-1)+ fib(index-2);
     }
 }
