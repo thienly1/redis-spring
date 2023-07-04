d2e673305a830db27318582177fa838d31f08177
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-07-04 15:34:55 +0200

    - modify to add expire time TTL to your data in redis

--- src/main/java/com/ly/redisspring/city/service/CityService.java
+++ src/main/java/com/ly/redisspring/city/service/CityService.java
@@ -2,6 +2,7 @@ package com.ly.redisspring.city.service;
 
 import com.ly.redisspring.city.client.CityClient;
 import com.ly.redisspring.city.dto.City;
+import org.redisson.api.RMapCacheReactive;
 import org.redisson.api.RMapReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.codec.TypedJsonJacksonCodec;
@@ -9,15 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class CityService {
 
     @Autowired
     private CityClient cityClient;
-    private RMapReactive<String, City> cityMap;
+    private RMapCacheReactive<String, City> cityMap;
 
     public  CityService(RedissonReactiveClient client){
-        this.cityMap = client.getMap("city", new TypedJsonJacksonCodec(String.class, City.class));
+        this.cityMap = client.getMapCache("city", new TypedJsonJacksonCodec(String.class, City.class));
     }
 
     /*
@@ -28,7 +31,7 @@ public class CityService {
     public Mono<City> getCity(final String zipCode){
         return this.cityMap.get(zipCode)  //get from cache
                 .switchIfEmpty(this.cityClient.getCity(zipCode)) //if empty, switch to source to get
-                .flatMap(c -> this.cityMap.fastPut(zipCode, c) //put it in cache
+                .flatMap(c -> this.cityMap.fastPut(zipCode, c, 10, TimeUnit.SECONDS) //put it in cache, then remove it after every 10 seconds
                         .thenReturn(c)                         //return value
                 );
     }
