b22e0210c855d9815526b59306cd9775056c386f
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-07-04 08:29:38 +0200

    - Spring Data Redis Test

--- src/test/java/com/ly/redisspring/RedisSpringApplicationTests.java
+++ src/test/java/com/ly/redisspring/RedisSpringApplicationTests.java
@@ -1,13 +1,33 @@
 package com.ly.redisspring;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.ReactiveValueOperations;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class RedisSpringApplicationTests {
+	@Autowired
+	private ReactiveStringRedisTemplate template;
 
-	@Test
-	void contextLoads() {
+	@RepeatedTest(3)
+	void springDataRedisTest() {
+		ReactiveValueOperations<String, String> valueOperations = this.template.opsForValue();
+		long before = System.currentTimeMillis();
+		Mono<Void> mono = Flux.range(1, 500_000)
+				.flatMap(i -> valueOperations.increment("user:1:visit")) //incr
+				.then();
+		StepVerifier.create(mono)
+				.verifyComplete();
+		long after = System.currentTimeMillis();
+		System.out.println((after-before) + " ms");
+
 	}
+
 
 }
