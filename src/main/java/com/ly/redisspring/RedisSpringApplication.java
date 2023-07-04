1ea8fc8c2e80512c83cd0ef2481c1060ccf5c93e
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-07-04 06:28:11 +0200

    Initial commit

---
+++ src/main/java/com/ly/redisspring/RedisSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ly.redisspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ly/redisspring/RedisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.redisspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
